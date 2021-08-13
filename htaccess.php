19a74f94095d1c639d5c8cc8a530b4917dcd160e
vasin777 <69838932+vasin777@users.noreply.github.com>
2021-08-13 15:03:52 +0300

    чпу
    чпу

--- htaccess.php
+++ htaccess.php
@@ -2,5 +2,5 @@ DirectoryIndex index.php
 Options -Indexes
 php_flag register_globals off
 RewriteEngine on
-# Ниже строка чпу благодаря ней урл будет выглядеть примерно так http://test-s.lok/-v465h
+# Ниже строка чпу благодаря ней урл будет выглядеть примерно так http://short-ur/-v465h
 RewriteRule ^-(.*) short_url/redirect.php?key=$1 [L]
