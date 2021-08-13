8af53fcee2c7f2099abf1f23935ffe268ab3e92b
vasin777 <69838932+vasin777@users.noreply.github.com>
2021-08-13 15:02:31 +0300

    index
    index

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-   // require_once '.htaccess';
+   // подключаем необходимые файлы для index стартовой страницы
     require_once("short_url/redirect.php");
     require_once("short_url/short_url.php"); //подключение конфигурационного файла
 
